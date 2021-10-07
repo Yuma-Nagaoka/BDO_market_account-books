bd6cc6ecb3f2f61a19df16d11f9d285b2d552dd9
YumaNagaoka <48959581+YumaNagaoka@users.noreply.github.com>
2021-10-07 21:57:27 +0900

    fix bugs No.23~26

--- api/lib/delete_data.rb
+++ api/lib/delete_data.rb
@@ -5,6 +5,7 @@ class Delete_data
     def proc(params)
         old_accountBook = []
         new_accountBook = []
+        bop = 0
 
         File.open("./outputs/AccountBooks/#{params[:date]}_accountBook.json") do |file|
             old_accountBook = JSON.load(file)
@@ -13,6 +14,14 @@ class Delete_data
         p "old", old_accountBook
         old_accountBook["items"].delete_at(params[:index]) #ここにおけるidは配列のindexと同じ
         new_accountBook = old_accountBook
+
+        #収支の更新
+        new_accountBook["items"].each do |item|
+            bop += item["accumulateMoneyCount"].delete(",").to_i if item["type"] == "販売"
+            bop -= item["accumulateMoneyCount"].delete(",").to_i if item["type"] == "購入"
+        end
+        new_accountBook.store("BOP", bop)
+        #
         
         File.open("./outputs/AccountBooks/#{params[:date]}_accountBook.json", "w") do |file|
             if new_accountBook.nil?
