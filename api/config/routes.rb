75435b8e105189199d6242467b61b04378b841ee
YumaNagaoka <48959581+YumaNagaoka@users.noreply.github.com>
2021-10-16 19:36:30 +0900

    def dummy
    ダミーデータ作成メソッドを定義

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -7,4 +7,6 @@ Rails.application.routes.draw do
   delete 'account_books/:date', to: 'account_books#destroy'
   patch 'account_books/:date', to: 'account_books#update'
   post 'credential_update', to: 'cookies#update'
+  get 'dummy', to: 'account_books#dummy'
+  get 'dummy_bop', to: 'account_books#dummy_bop'
 end
