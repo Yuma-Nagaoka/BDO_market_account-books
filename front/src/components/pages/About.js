52e3d7f32dbc771bd8aed794d2bdcfc49526546e
YumaNagaoka <48959581+YumaNagaoka@users.noreply.github.com>
2021-11-23 15:55:41 +0900

    windowサイズによってホーム画面のmaskの高さを変更

--- front/src/components/pages/About.js
+++ front/src/components/pages/About.js
@@ -8,17 +8,22 @@ import Typography from '@mui/material/Typography';
 import { Link, useHistory } from 'react-router-dom'
 
 
-
 export default function About() {
     const history = useHistory();
     const handleClick = () => {
         history.push('/AccountBooks');
     }
+//windowサイズによって初期のmaskの高さを決める
+    const [maskHeight, setMaskHeight] = window.innerHeight > 900 ? React.useState({height: '75%'}) : React.useState({height: '60%'});
+//windowサイズが変更されるとmaskの高さも動的に変更する
+    window.addEventListener('resize', function () {
+        window.innerHeight > 900 ? setMaskHeight({height: '75%'}) : setMaskHeight({height: '60%'})
+        // console.log(maskHeight)
+    }, true);
 
     return (
         <div className="backGround">
-            <div className="mask">
-            <div className="container" style={{paddingBottom: '0px'}}>
+            <div className="mask" style={maskHeight}>
                 <Typography variant="h5" component="div" sx={{ fontSize: 30, marginTop: '20px', color: 'white'}}>
                 MMORPG 黒い砂漠でお金を貯めたいあなたへ
                 <br /><br />
@@ -30,7 +35,6 @@ export default function About() {
                         始める</Button>
                 </div>
                 <br />
-            </div>
             </div>
         </div>
     );
